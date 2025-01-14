6d123c22f25dd754cb1bc8dd09ea96fd9e9f555d
secrett2633 <secrett2633@kakao.com>
2025-01-14 23:29:50 +0900

    fix ports

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -6,7 +6,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.core.settings")
 # Uvicorn ASGI worker 사용
 worker_class = "uvicorn.workers.UvicornWorker"
 
-bind = "0.0.0.0:8000"
 timeout = 60
 max_requests = 2000
 max_requests_jitter = 50
